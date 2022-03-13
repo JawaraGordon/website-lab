d5b25de08cfd2b976f98d747b3fa9959fc6bc055
JawaraGordon <jawaragordon@icloud.com>
2022-03-13 09:11:11 -0400

    created index.js linked to index.html

---
+++ index.js
@@ -0,0 +1,15 @@
+const header = document.getElementById("myHeader");
+header.style.fontSize = "400px";
+
+const title1 = document.getElementById("title1");
+title1.style.fontSize = "150px";
+
+const title1 = document.getElementById("title2");
+title2.style.fontSize = "100px";
+
+const title1 = document.getElementById("title3");
+title3.style.fontSize = "50px";
+
+const title1 = document.getElementById("title4");
+title4.style.fontSize = "25px";
+
